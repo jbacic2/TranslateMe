1a8814f35f7abcd03db333a942ddedd3cf683454
derek <derek@llamabagel.ca>
2019-01-27 07:50:52 -0500

    Move fragment to ui package

--- app/src/main/java/com/example/joyce/translateme/ui/LoginFragment.kt
+++ app/src/main/java/com/example/joyce/translateme/ui/LoginFragment.kt
@@ -1,4 +1,4 @@
-package com.example.joyce.translateme
+package com.example.joyce.translateme.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.joyce.translateme.R
 import kotlinx.android.synthetic.main.fragment_login.*
 
 
